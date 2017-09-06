73c0a345b5e87a371d8821e0b462cc94369fec72
Cody Kochmann <kochmanncody@gmail.com>
2017-09-06 16:37:19 -0400

    added itemgetter

---
+++ generators/itemgetter.py
@@ -0,0 +1,23 @@
+from collections import deque
+from operator import itemgetter
+
+def itemgetter(iterable, indexes):
+    ''' same functionality as operator.itemgetter except, this one supports
+        both positive and negative indexing of generators as well '''
+    assert type(indexes)==tuple, 'indexes needs to be a tuple of ints'
+    assert all(type(i)==int for i in indexes), 'indexes needs to be a tuple of ints'
+    positive_indexes=[i for i in indexes if i>=0]
+    negative_indexes=[i for i in indexes if i<0]
+    out = {}
+    if len(negative_indexes):
+        # if there are any negative indexes
+        negative_index_buffer = deque(maxlen=min(indexes)*-1)
+        for i,x in enumerate(iterable):
+            if i in positive_indexes:
+                out[i]=x
+            negative_index_buffer.append(i)
+        out.update({ni:negative_index_buffer[ni] for ni in negative_indexes})
+    else:
+        # if just positive results
+        out.update({i:x for i,x in enumerate(iterable) if i in positive_indexes})
+    return itemgetter(*indexes)(out)
