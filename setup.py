c428b9794b97b5caa0360a0f1e56cd5678f0124b
Cody Kochmann <kochmanncody@gmail.com>
2017-09-06 15:21:09 -0400

    autocommit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'generators',
+  packages = ['generators'], # this must be the same as the name above
+  version = '2017.9.6',
+  description = 'collection of helpful generators that should have been in itertools',
+  author = 'Cody Kochmann',
+  author_email = 'kochmanncody@gmail.com',
+  url = 'https://github.com/CodyKochmann/generators',
+  download_url = 'https://github.com/CodyKochmann/generators/tarball/2017.9.6',
+  keywords = ['generators', 'iter', 'itertools', 'combinations', 'chain', 'iterate', 'gen'],
+  classifiers = [],
+)
