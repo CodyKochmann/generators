70ed29e0dcdd7a0aabfcb4f453160df38806c5b4
Cody Kochmann <kochmanncody@gmail.com>
2017-09-06 16:27:54 -0400

    first release of generators

--- generators/__init__.py
+++ generators/__init__.py
@@ -3,7 +3,22 @@
 # @Date:   2017-05-04 17:11:54
 # @Last Modified by:   ckochman
 # @Last Modified time: 2017-05-04 18:06:21
-'''
+
+import sys
+
+def script_path(include_name=False):
+    # returns the full path of the script containing this snippet
+    from os import path
+    full_path = path.realpath(__file__)
+    if include_name:
+        return(full_path)
+    else:
+        full_path = "/".join( full_path.split("/")[0:-1] ) + "/"
+        return(full_path)
+
+sys.path.append(script_path())
+
+
 from average import average
 from counter import counter
 from read_file import read_file
@@ -13,4 +28,4 @@ from timer import timer
 from fork import fork
 from unfork import unfork
 from multi_ops import multi_ops
-'''
+
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'generators',
   packages = ['generators'], # this must be the same as the name above
-  version = '2017.9.6.2',
+  version = '2017.9.6.3',
   description = 'collection of helpful generators that should have been in itertools',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/generators',
-  download_url = 'https://github.com/CodyKochmann/generators/tarball/2017.9.6.2',
+  download_url = 'https://github.com/CodyKochmann/generators/tarball/2017.9.6.3',
   keywords = ['generators', 'iter', 'itertools', 'combinations', 'chain', 'iterate', 'gen'],
   classifiers = [],
 )
