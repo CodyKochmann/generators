2b942333b1e3d0fa22a00fe06c685917fddeca29
Cody Kochmann <kochmanncody@gmail.com>
2017-09-09 08:56:07 -0400

    added chunks

---
+++ generators/chunks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Cody Kochmann
+# @Date:   2017-05-04 18:04:39
+# @Last Modified by:   Cody Kochmann
+
+from collections import deque
+
+
+def chunks(iterable, chunk_size, output_type=tuple):
+    ''' returns chunks of an iterable '''
+    chunk = deque(maxlen=chunk_size)
+    for i in iterable:
+        chunk.append(i)
+        if len(chunk) == chunk_size:
+            yield output_type(chunk)
+            chunk.clear()
+    if len(chunk):
+        yield output_type(chunk)
