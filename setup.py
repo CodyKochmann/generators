387b4520c98bab8f3213ac57f2f60763da4e9a00
Cody Kochmann <kochmanncody@gmail.com>
2017-09-24 17:38:45 -0400

    added performance_tools.runs_per_second

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'generators',
   packages = ['generators'], # this must be the same as the name above
-  version = '2017.9.23.2',
+  version = '2017.9.24',
   description = 'collection of helpful generators that should have been in itertools',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/generators',
-  download_url = 'https://github.com/CodyKochmann/generators/tarball/2017.9.23.2',
+  download_url = 'https://github.com/CodyKochmann/generators/tarball/2017.9.24',
   keywords = ['generators', 'iter', 'itertools', 'combinations', 'chain', 'iterate', 'gen'],
   classifiers = [],
 )
