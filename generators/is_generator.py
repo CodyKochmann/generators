6ff05fbb72d501e565520728dab366435629f1ac
Cody Kochmann <kochmanncody@gmail.com>
2017-09-30 22:12:25 -0400

    added an is_iterable function for clean iterable validation

---
+++ generators/is_generator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Author: Cody Kochmann
+# @Date:   2017-09-30 22:09:35
+# @Last Modified by:   Cody Kochmann
+# @Last Modified time: 2017-09-30 22:11:35
+
+def is_iterable(target):
+    ''' returns true if the given argument is iterable '''
+    if any(i in ('next', '__next__', '__iter__') for i in dir(target):
+        return True
+    else:
+        try:
+            iter(target)
+            return True
+        except:
+            return False
+
