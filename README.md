c858856d2560832aadef38c16a2b2cd5d935a0f8
Cody Kochmann <kochmanncody@gmail.com>
2017-10-17 16:16:32 -0400

    added boolean based chunking to chunks

--- README.md
+++ README.md
@@ -72,14 +72,24 @@ def chain(*args):
 
 ### generators.chunks
 ```python
-def chunks(iterable, chunk_size, output_type=tuple):
-    ''' returns chunks of an iterable '''
-    chunk = deque(maxlen=chunk_size)
-    for i in iterable:
-        chunk.append(i)
-        if len(chunk) == chunk_size:
-            yield output_type(chunk)
-            chunk.clear()
+def chunks(stream, chunk_size, output_type=tuple):
+    ''' returns chunks of a stream '''
+    if callable(chunk_size):
+        ''' chunk_size is acting as a separator function '''
+        seperator = chunk_size
+        chunk = deque()
+        for i in stream:
+            if seperator(i) and len(chunk):
+                yield output_type(chunk)
+                chunk.clear()
+            chunk.append(i)
+    else:
+        chunk = deque(maxlen=chunk_size)
+        for i in stream:
+            chunk.append(i)
+            if len(chunk) == chunk_size:
+                yield output_type(chunk)
+                chunk.clear()
     if len(chunk):
         yield output_type(chunk)
 ```
--- generators/chunks.py
+++ generators/chunks.py
@@ -2,18 +2,39 @@
 # -*- coding: utf-8 -*-
 # @Author: Cody Kochmann
 # @Date:   2017-09-09
-# @Last Modified by:   Cody Kochmann
+# @Last Modified 2017-10-17
 
 from collections import deque
 
 
-def chunks(iterable, chunk_size, output_type=tuple):
-    ''' returns chunks of an iterable '''
-    chunk = deque(maxlen=chunk_size)
-    for i in iterable:
-        chunk.append(i)
-        if len(chunk) == chunk_size:
-            yield output_type(chunk)
-            chunk.clear()
+def chunks(stream, chunk_size, output_type=tuple):
+    ''' returns chunks of a stream '''
+    if callable(chunk_size):
+        ''' chunk_size is acting as a separator function '''
+        seperator = chunk_size
+        chunk = deque()
+        for i in stream:
+            if seperator(i) and len(chunk):
+                yield output_type(chunk)
+                chunk.clear()
+            chunk.append(i)
+    else:
+        chunk = deque(maxlen=chunk_size)
+        for i in stream:
+            chunk.append(i)
+            if len(chunk) == chunk_size:
+                yield output_type(chunk)
+                chunk.clear()
     if len(chunk):
         yield output_type(chunk)
+
+
+if __name__ == '__main__':
+    l = list(range(30))
+    print(list(chunks(l, 2)))
+    print(list(chunks(l, 15)))
+    print(list(chunks(l, 20)))
+    print(list(chunks(l, lambda i:i==15)))
+    print(list(chunks(l, lambda i:i%5==0)))
+    print(list(chunks(l, lambda i:str(i).startswith('1'))))
+
