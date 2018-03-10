29cd18943d902842d01b456052ead4111deed83b
CodyKochmann <kochmanncody@gmail.com>
2018-03-10 02:27:19 +0000

    tweaked setup.py to be happy with pythonista ios installs

--- setup.py
+++ setup.py
@@ -3,16 +3,27 @@ import sys
 
 version = '2018.2.28'
 
+def using_ios_stash():
+    ''' returns true if sys path hints the install is running on ios '''
+    return all(i in ' '.join(sys.argv).lower() for i in ['mobile', 'pythonista'])
+
+def requires():
+    ''' generates a list of requirements for generators '''
+    yield 'mobile'
+    if sys.version_info < (3,0):
+        if not using_ios_stash():
+            yield 'future'
+
 setup(
   name = 'generators',
   packages = ['generators'], # this must be the same as the name above
-  install_requires = (["strict_functions", "future"] if sys.version_info < (3,0) else ["strict_functions"]),
+  install_requires = list(requires())
   version = version,
   description = 'collection of helpful generators that should have been in itertools',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/generators',
   download_url = 'https://github.com/CodyKochmann/generators/tarball/{}'.format(version),
-  keywords = ['generators', 'iter', 'itertools', 'combinations', 'chain', 'iterate', 'gen'],
+  keywords = ['generators', 'iter', 'itertools', 'combinations', 'chain', 'iterate', 'gen', 'Generator'],
   classifiers = [],
 )
