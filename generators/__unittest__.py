7ce70a3eb0bbafc1a31348eadd94a16401ceb4b1
codykochmann <kochmanncody@gmail.com>
2018-11-04 10:22:45 -0500

    added unittests to better validate changes in chunks

---
+++ generators/__unittest__.py
@@ -0,0 +1,11 @@
+import collections
+import os
+import sys
+import unittest
+
+sys.path.append(os.path.dirname(__file__))
+
+from chunks import Test_chunks
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
--- generators/chunks.py
+++ generators/chunks.py
@@ -4,6 +4,8 @@
 # @Date:   2017-09-09
 # @Last Modified 2017-10-17
 
+import unittest
+
 try:
     from itertools import zip_longest
 except ImportError:
@@ -23,7 +25,6 @@ def chunks(stream, chunk_size, output_type=tuple):
     assert (isinstance(chunk_size, int) and chunk_size > 0) or callable(chunk_size), 'chunks needs chunk_size to be a positive int or callable'
     assert callable(output_type), 'chunks needs output_type to be callable'
     if callable(chunk_size):
-        print('callable')
         ''' chunk_size is acting as a separator function '''
         for chunk in chunk_on(stream, chunk_size, output_type):
             yield chunk
@@ -46,14 +47,78 @@ del chunk_on
 del map
 del strict_globals
 
+#==============================================================
+# Test code below:
+#==============================================================
+
+class Test_chunks(unittest.TestCase):
+    ''' this runs tests to verify behavior of generators.chunks '''
+    def test_chunks_basic(self):
+        ''' test the basic usage of chunks '''
+        l = list(range(30))
+
+        self.assertEqual(
+            list(chunks(l, 2)),
+            [(0, 1), (2, 3), (4, 5), (6, 7), (8, 9), (10, 11), (12, 13), (14, 15), (16, 17), (18, 19), (20, 21), (22, 23), (24, 25), (26, 27), (28, 29)]
+        )
+        self.assertEqual(
+            list(chunks(l, 15)),
+            [(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14), (15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29)]
+        )
+        self.assertEqual(
+            list(chunks(l, 20)),
+            [(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19), (20, 21, 22, 23, 24, 25, 26, 27, 28, 29)]
+        )
+        self.assertEqual(
+            list(chunks(l, 2, set)),
+            [{0, 1}, {2, 3}, {4, 5}, {6, 7}, {8, 9}, {10, 11}, {12, 13}, {14, 15}, {16, 17}, {18, 19}, {20, 21}, {22, 23}, {24, 25}, {26, 27}, {28, 29}]
+        )
+        self.assertEqual(
+            list(chunks(l, 15, set)),
+            [{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}, {15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29}]
+        )
+        self.assertEqual(
+            list(chunks(l, 20, set)),
+            [{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}, {20, 21, 22, 23, 24, 25, 26, 27, 28, 29}]
+        )
+        self.assertEqual(
+            list(chunks(l, lambda i:i==15)),
+            [(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14), (15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29)]
+        )
+        self.assertEqual(
+            list(chunks(l, lambda i:i%5==0)),
+            [(0, 1, 2, 3, 4), (5, 6, 7, 8, 9), (10, 11, 12, 13, 14), (15, 16, 17, 18, 19), (20, 21, 22, 23, 24), (25, 26, 27, 28, 29)]
+        )
+        self.assertEqual(
+            list(chunks(l, lambda i:str(i).startswith('1'))),
+            [(0,), (1, 2, 3, 4, 5, 6, 7, 8, 9), (10,), (11,), (12,), (13,), (14,), (15,), (16,), (17,), (18,), (19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29)]
+        )
+
+    def test_chunks_ranged(self, test_range=10, chunk_size=2):
+        pipe = chunks(range(test_range), chunk_size)
+        for first_digit in range(0, test_range, chunk_size):
+            expected_chunk = tuple((i for i in range(first_digit, first_digit+chunk_size) if i < test_range))
+            chunk = next(pipe)
+            self.assertTrue(isinstance(chunk, tuple), msg='incorrect chunk type')
+            self.assertEqual(len(expected_chunk), len(chunk), msg='incorrect chunk size')
+            self.assertEqual(expected_chunk, chunk, msg='invalid chunk')
+
+    def test_chunks_multi_ranged(self):
+        for chunk_size in range(1, 30):
+            self.test_chunks_ranged(test_range=1000, chunk_size=chunk_size)
+
+    def test_chunks_output_type(self):
+        for t in (tuple, list, str):
+            self.assertEqual(type(next(chunks(range(10), 2, output_type=t))), t, 'invalid custom output type')
+
+    def test_chunks_input_validation(self):
+        with self.assertRaises(AssertionError):
+            list(chunks(1, 2))
+        with self.assertRaises(AssertionError):
+            list(chunks(range(10), 'hi'))
+        with self.assertRaises(AssertionError):
+            list(chunks(range(10), 2, 'fishsticks'))
+
+
 if __name__ == '__main__':
-    l = list(range(30))
-    print(list(chunks(l, 2)))
-    print(list(chunks(l, 15)))
-    print(list(chunks(l, 20)))
-    print(list(chunks(l, 2, set)))
-    print(list(chunks(l, 15, set)))
-    print(list(chunks(l, 20, set)))
-    print(list(chunks(l, lambda i:i==15)))
-    print(list(chunks(l, lambda i:i%5==0)))
-    print(list(chunks(l, lambda i:str(i).startswith('1'))))
+    unittest.main(verbosity=2)
