7e4b9de88359054738521f820fcc3da50bd12da5
codykochmann <kochmanncody@gmail.com>
2018-11-04 11:12:22 -0500

    added unittests for apply_to_last.py

--- generators/__unittest__.py
+++ generators/__unittest__.py
@@ -6,6 +6,7 @@ import unittest
 sys.path.append(os.path.dirname(__file__))
 
 from chunks import Test_chunks
+from apply_to_last import Test_apply_to_last
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
