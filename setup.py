a6f7aa1f116835fd8ee881921023dab7a8d6388b
Cody Kochmann <kochmanncody@gmail.com>
2019-05-04 12:13:49 -0400

    added a .run() syntax for more user friendly .consume() usages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 import sys
 import os
 
-version = '2019.5.1'
+version = '2019.5.4'
 
 def using_ios_stash():
     ''' returns true if sys path hints the install is running on ios '''
